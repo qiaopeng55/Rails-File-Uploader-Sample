0f567e54982f6427d1991d3973003abb0101e95f
Claudio Lassala <claudiolassala@gmail.com>
2011-03-10 22:04:48 -0600

    added some more actions

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -15,4 +15,17 @@ class FoldersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @folder = Folder.find(params[:id])
+  end
+
+  def update
+    @folder = Folder.find(params[:id])
+    if @folder.update_attributes(params[:folder])
+      redirect_to folders_path
+    else
+      render 'edit'
+    end
+  end
 end
